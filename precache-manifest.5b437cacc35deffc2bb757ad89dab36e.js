6a2b11366677e9963c687b019095bc861c941f24
Mike <shadowmare@windowslive.com>
2019-04-23 12:02:24 +0200

    Updates

---
+++ precache-manifest.5b437cacc35deffc2bb757ad89dab36e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "418a44675423578f90a4266f79575634",
+    "url": "/app-rr/static/media/header3.418a4467.jpg"
+  },
+  {
+    "revision": "5090cdfced2f1b0b42f43ced1c32911b",
+    "url": "/app-rr/static/media/header2.5090cdfc.jpg"
+  },
+  {
+    "revision": "816579d6c6705616afc808a61facada8",
+    "url": "/app-rr/static/media/header1.816579d6.jpg"
+  },
+  {
+    "revision": "9347ed20ee693a8d157a",
+    "url": "/app-rr/static/js/runtime~main.fe775957.js"
+  },
+  {
+    "revision": "9a076c2ba99f3e156abc",
+    "url": "/app-rr/static/js/main.b05299bb.chunk.js"
+  },
+  {
+    "revision": "d9d505f6d64303e766bd",
+    "url": "/app-rr/static/js/2.d2e2a467.chunk.js"
+  },
+  {
+    "revision": "9a076c2ba99f3e156abc",
+    "url": "/app-rr/static/css/main.8294d4c7.chunk.css"
+  },
+  {
+    "revision": "382277f3b4d7c77b9307c3970424d828",
+    "url": "/app-rr/index.html"
+  }
+];
